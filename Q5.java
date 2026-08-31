461c72bb6a895a80d49b0d5111d5d97197a21499
99bytes <ayushsinha6206@gmail.com>
2026-08-31 16:06:35 +0530

    Valid Anagram

---
+++ Q5.java
@@ -0,0 +1,29 @@
+class Q5 {
+    public boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        // Count characters of s
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        // Count characters of t
+        for (char c : t.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        // Check frequencies
+        for (int value : map.values()) {
+            if (value % 2 != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
