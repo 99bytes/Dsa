7b3d03f21a8a106dceae587caa4e62475863a0ef
99bytes <ayushsinha6206@gmail.com>
2026-09-02 13:59:51 +0530

    Remove outer Parenthesis

---
+++ String/Q8.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        int depth =0 ;
+        StringBuilder sb = new StringBuilder();
+
+        for(char ch : s.toCharArray()){
+            if(ch =='('){
+                if(depth>0){
+                    sb.append(ch);
+                }
+                depth++;
+            }
+
+            if(ch== ')'){
+                depth--;
+                if(depth>0){
+                    sb.append(ch);
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
